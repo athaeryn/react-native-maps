154be9e228306d067630f76908a630936833aeca
Mike Anderson <mike@mkndrsn.com>
2016-08-29 12:32:59 -0500

    Implement inset props on iOS

--- ios/AirMaps/AIRMapManager.h
+++ ios/AirMaps/AIRMapManager.h
@@ -8,7 +8,16 @@
  */
 
 #import "RCTViewManager.h"
+#import <MapKit/MapKit.h>
 
 @interface AIRMapManager : RCTViewManager
+
+@property (nonatomic, assign) NSInteger insetTop;
+@property (nonatomic, assign) NSInteger insetBottom;
+@property (nonatomic, assign) NSInteger insetLeft;
+@property (nonatomic, assign) NSInteger insetRight;
+
+- (UIEdgeInsets) getEdgeInsets;
++ (MKMapRect) getMKMapRectFromMKCoordinateRegion: (MKCoordinateRegion)region;
 
 @end
